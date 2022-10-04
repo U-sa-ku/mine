41f32aea1e396f015c4c0f4004991596634e5f62
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2022-10-04 14:29:19 +0900

    スタイル調整

--- pages/sidekick/_slug/index.vue
+++ pages/sidekick/_slug/index.vue
@@ -157,7 +157,7 @@ $mainvisualAnimationStartDelay: 0s;
     transition: 1s $mainvisualAnimationStartDelay + 1s;
     @media (max-width: 767px) {
       width: 90%;
-      font-size: 9.6vw;
+      font-size: 9vw;
       transform: translateY(10vw) rotate(-90deg);
     }
     &.jsAnimation {
@@ -185,7 +185,7 @@ $mainvisualAnimationStartDelay: 0s;
     transform-origin: 0% 100%;
     transition: 1s $mainvisualAnimationStartDelay + 1.5s;
     @media (max-width: 767px) {
-      font-size: 9.6vw;
+      font-size: 9vw;
     }
     &.jsAnimation {
       opacity: 1;
