ba31a29cae70611a3c2252a73923b5aca1ad8147
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2022-11-16 16:16:04 +0900

    test

--- components/sections/Photograph.vue
+++ components/sections/Photograph.vue
@@ -106,13 +106,9 @@ export default {
     }
   }
   &__image {
-    max-width: 100%;
-    max-height: 100%;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    z-index: 1;
-    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
     pointer-events: none;
   }
   .swiper-button {
