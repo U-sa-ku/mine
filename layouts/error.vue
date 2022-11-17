54ec5e036388ee7b0a1273538b6dd770976a2a65
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2022-11-17 10:18:01 +0900

    test

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,20 @@
 <template>
   <main class="errorMessage">
     <div class="errorMessage__wrapper">
-      <p class="errorMessage__message">error</p>
+      <p class="errorMessage__statusCode">{{ error.statusCode }}</p>
+      <p class="errorMessage__message">{{ error.message }}</p>
     </div>
   </main>
 </template>
 
 <script>
 export default {
-  props: ['error'],
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
   mounted() {
     this.$nuxt.$emit("onLoad", true)
     this.$nuxt.$emit("initShowSiteHeader")
@@ -19,9 +25,12 @@ export default {
 <style lang="scss" scoped>
 .errorMessage {
   height: calc(100vh - 149px);
+  margin-bottom: -300px;
   padding-top: 70px;
+  box-sizing: content-box;
   @media (max-width: 999px) {
     height: calc(100vh - 89px);
+    margin-bottom: -200px;
     padding-top: 40px;
   }
   &__wrapper {
@@ -36,17 +45,21 @@ export default {
     top: 0px;
     z-index: 1;
   }
+  &__statusCode {
+    font-size: 8rem;
+    letter-spacing: 0.1em;
+    line-height: 1;
+    margin-bottom: 10px;
+    @media (max-width: 999px) {
+      font-size: 4rem;
+    }
+  }
   &__message {
     font-size: 4rem;
     letter-spacing: 0.1em;
     @media (max-width: 999px) {
-      font-size: 2.6rem;
+      font-size: 2rem;
     }
   }
-}
-</style>
-<style lang="scss">
-.contentsWrapper {
-  padding-bottom: 0px !important;
 }
 </style>
