6fb7c6d4a13ccdb2d624754848452e793430feee
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2022-11-17 16:58:14 +0900

    元に戻す

--- layouts/no-site-header.vue
+++ layouts/no-site-header.vue
@@ -10,17 +10,6 @@
 <script>
 export default {
   name: 'no-site-header',
-  async asyncData({ params }) {
-    const workbox = await window.$workbox;
-
-    if (workbox) {
-      workbox.addEventListener('installed', (event) => {
-        if (event.isUpdate) {
-          alert('update')
-        }
-      });
-    }
-  },
   data() {
     return {
       isLoaded: false
